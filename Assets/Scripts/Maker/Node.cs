7897cfc79013f6aeebe4fa586f83139c7ba93ef8
Marteaufou <martoxiss@gmail.com>
2016-03-17 15:44:54 +0100

    Revert "Revert "Dossier propre GitHub""
    This reverts commit a0dd2a93e57d0fc02fd5fce19e58404859c11f92.

---
+++ Assets/Scripts/Maker/Node.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Node {
+
+    public int nodePosX;
+    public int nodePosY;
+    public GameObject vis;
+    public MeshRenderer tileRenderer;
+    public bool isWalkable;
+    public LevelEditor.Level_Object placeObj;
+    public List<LevelEditor.Level_Object> stackdObjs = new List<LevelEditor.Level_Object>();
+    //public LevelEditor.Level_WallObj wallObj;
+}
